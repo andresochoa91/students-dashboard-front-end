5b835fb0026a29a1eef272ad9d06c19334b7ff05
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-09 14:53:48 -0800

    Adding fetch call

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import { AuthStore } from './contexts/AuthContext';
@@ -14,7 +14,22 @@ import HomePage from './homePage/HomePage';
 import GlobalStyles from '../styles/Global';
 import 'antd/dist/antd.css';
 
+
 const App = () => {
+
+  useEffect(() => {
+    fetch("http://localhost:3000", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(console.log)
+    .catch(console.error);
+  }, []);
+  
   return (
     <BrowserRouter>
       <CookiesProvider>
