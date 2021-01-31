50b983371b32907bbfef105c9d6df936cb3b9bc9
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-30 23:20:24 -0800

    Changing class component to functional component

--- src/components/homePage/dashboard/staffDashboard/studentsTrackingReport/TrackingReport.js
+++ src/components/homePage/dashboard/staffDashboard/studentsTrackingReport/TrackingReport.js
@@ -1,118 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'antd';
 
 
-class TrackingReport extends React.Component {
+const TrackingReport = () => {
 
-  state = {
-    sortedInfo: null
+  const [ sortedInfo, setSortedInfo ] = useState(); 
+
+  const handleChange = (pagination, filters, sorter) => {
+    console.log('Various parameters', pagination, filters, sorter);
+    setSortedInfo(sorter);
   };
 
-  render() {
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      course: 'High Noon',
+      units: '+',
+      assignments: 1,
+      githubLink: 1,
+      status: 1
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      course: 'High Noon',
+      units: '+',
+      assignments: 1,
+      githubLink: 1,
+      status: 1
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      course: 'High Noon',
+      units: '+',
+      assignments: 1,
+      githubLink: 1,
+      status: 1
+    },
+    {
+      key: '4',
+      name: 'Jim Red',
+      course: 'High Noon',
+      units: '+',
+      assignments: 1,
+      githubLink: 1,
+      status: 1
+    },
+  ];
 
-    const handleChange = (pagination, filters, sorter) => {
-      console.log('Various parameters', pagination, filters, sorter);
-      this.setState({
-        sortedInfo: sorter
-      });
-    };
+  let sI = sortedInfo;
+  sI = sortedInfo || {};
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortOrder: sI.columnKey === 'name' && sI.order,
+      ellipsis: true,
+    },
+    {
+      title: 'Course',
+      dataIndex: 'course',
+      key: 'course',
+      ellipsis: true,
+    },
+    {
+      title: 'Units',
+      dataIndex: 'units',
+      key: 'units',
+      ellipsis: true,
+    },
+    {
+      title: 'Assignments',
+      dataIndex: 'assignments',
+      key: 'assignments',
+      ellipsis: true,
+    },
+    {
+      title: 'Github Link',
+      dataIndex: 'githubLink',
+      key: 'githubLink',
+      ellipsis: true,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      ellipsis: true,
+    },
+  ];
 
-    const data = [
-      {
-        key: '1',
-        name: 'John Brown',
-        course: 'High Noon',
-        units: '+',
-        assignments: 1,
-        githubLink: 1,
-        status: 1
-      },
-      {
-        key: '2',
-        name: 'Jim Green',
-        course: 'High Noon',
-        units: '+',
-        assignments: 1,
-        githubLink: 1,
-        status: 1
-      },
-      {
-        key: '3',
-        name: 'Joe Black',
-        course: 'High Noon',
-        units: '+',
-        assignments: 1,
-        githubLink: 1,
-        status: 1
-      },
-      {
-        key: '4',
-        name: 'Jim Red',
-        course: 'High Noon',
-        units: '+',
-        assignments: 1,
-        githubLink: 1,
-        status: 1
-      },
-    ];
-
-    let { sortedInfo } = this.state;
-    sortedInfo = sortedInfo || {};
-    
-    const columns = [
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
-        ellipsis: true,
-      },
-      {
-        title: 'Course',
-        dataIndex: 'course',
-        key: 'course',
-        ellipsis: true,
-      },
-      {
-        title: 'Units',
-        dataIndex: 'units',
-        key: 'units',
-        ellipsis: true,
-      },
-      {
-        title: 'Assignments',
-        dataIndex: 'assignments',
-        key: 'assignments',
-        ellipsis: true,
-      },
-      {
-        title: 'Github Link',
-        dataIndex: 'githubLink',
-        key: 'githubLink',
-        ellipsis: true,
-      },
-      {
-        title: 'Status',
-        dataIndex: 'status',
-        key: 'status',
-        ellipsis: true,
-      },
-    ];
-    return (
-      <>
-        <Table 
-          columns={columns} 
-          dataSource={data} 
-          onChange={handleChange} 
-          expandable={{
-            expandedRowRender: () => <p style={{ margin: 0 }}>Helllo</p>,
-            rowExpandable: record => record.name !== 'Not Expandable',
-          }}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <Table 
+        columns={columns} 
+        dataSource={data} 
+        onChange={handleChange} 
+        expandable={{
+          expandedRowRender: () => <p style={{ margin: 0 }}>Hello</p>,
+          rowExpandable: record => record.name !== 'Not Expandable',
+        }}
+      />
+    </>
+  );
 }
 
 export default TrackingReport;
