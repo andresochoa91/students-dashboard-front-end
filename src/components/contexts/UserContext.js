d6b3aa4a568883e95e158dfda9e7e343b8626e4c
andresochoa91 <andres_ochoa91@hotmail.com>
2021-02-15 22:29:00 -0800

    Standard indentation 1

--- src/components/contexts/UserContext.js
+++ src/components/contexts/UserContext.js
@@ -7,41 +7,41 @@ import _ from "lodash";
 const Context = React.createContext();
 
 export const UserStore = ({ children }) => {
-  const [cookies, setCookie] = useCookies(['auth_token']);
-  const [authToken, setAuthToken,] = useState(cookies['auth_token']);
-  const [userInfo, setUserInfo] = useState(null);
+	const [cookies, setCookie] = useCookies(['auth_token']);
+	const [authToken, setAuthToken,] = useState(cookies['auth_token']);
+	const [userInfo, setUserInfo] = useState(null);
 
-  useEffect(() => {
-    if (authToken && _.isEmpty(cookies)) {
-      setCookie('auth_token', authToken.token);
-      setUserInfo(authToken.info);
-    }
-  }, [authToken])
+	useEffect(() => {
+		if (authToken && _.isEmpty(cookies)) {
+		setCookie('auth_token', authToken.token);
+		setUserInfo(authToken.info);
+		}
+	}, [authToken])
 
-  useEffect(() => {
-    if (!_.isEmpty(cookies)) {
-      const getData = async () => {
-        const response = await fetch('https://forked-student-dashboard.herokuapp.com/user', {
-          method: 'POST',
-          mode: 'cors',
-          credentials: 'include',
-          headers: { 'Content-Type': 'application/json', 'Authorization': authToken }
-        });
-        const data = await response.json();
+	useEffect(() => {
+		if (!_.isEmpty(cookies)) {
+			const getData = async () => {
+				const response = await fetch('https://forked-student-dashboard.herokuapp.com/user', {
+					method: 'POST',
+					mode: 'cors',
+					credentials: 'include',
+					headers: { 'Content-Type': 'application/json', 'Authorization': authToken }
+				});
+				const data = await response.json();
 
-        setUserInfo(data);
-      }
-      getData();
-    }
-  }, [authToken]);
+				setUserInfo(data);
+			};
+			getData();
+		}
+	}, [authToken]);
 
-  return (
-    <Context.Provider
-      value={[authToken, setAuthToken, userInfo, setUserInfo]}
-    >
-      {children}
-    </Context.Provider>
-  )
+	return (
+		<Context.Provider
+			value={[authToken, setAuthToken, userInfo, setUserInfo]}
+		>
+			{children}
+		</Context.Provider>
+	)
 }
 
 export default Context;
--- src/components/homePage/assignments/staff/createInstructions/CreateInstructions.js
+++ src/components/homePage/assignments/staff/createInstructions/CreateInstructions.js
@@ -2,60 +2,59 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 
 const modules = {
-  toolbar: [
-    [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-    [{ size: [] }],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{ 'list': 'ordered' }, { 'list': 'bullet' },
-    { 'indent': '-1' }, { 'indent': '+1' }],
-    ['link', 'image', 'video'],
-    ['clean']
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  }
+	toolbar: [
+		[{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+		[{ size: [] }],
+		['bold', 'italic', 'underline', 'strike', 'blockquote'],
+		[{ 'list': 'ordered' }, { 'list': 'bullet' },
+		{ 'indent': '-1' }, { 'indent': '+1' }],
+		['link', 'image', 'video'],
+		['clean']
+	],
+	clipboard: {
+		// toggle to add extra line breaks when pasting HTML:
+		matchVisual: false,
+	}
 }
 /* 
  * Quill editor formats
  * See https://quilljs.com/docs/formats/
  */
 const formats = [
-  'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image', 'video'
+	'header', 'font', 'size',
+	'bold', 'italic', 'underline', 'strike', 'blockquote',
+	'list', 'bullet', 'indent',
+	'link', 'image', 'video'
 ]
 
 const progressTypes = {
-  0: 'instructions', 1: 'resources', 2: 'submission', 3: 'done'
+	0: 'instructions', 1: 'resources', 2: 'submission', 3: 'done'
 }
 
 const CreateInstructions = ({ dispatch, step }) => {
-  const [value, setValue] = useState('');
+	const [value, setValue] = useState('');
 
-  const onChange = (content, delta, source, editor) => {
-    const html = editor.getHTML()
-    setValue(html)
-    dispatch({
-      type: progressTypes[step],
-      payload: { field: progressTypes[step], value: html },
-    })
-  }
+	const onChange = (content, delta, source, editor) => {
+		const html = editor.getHTML()
+		setValue(html)
+		dispatch({
+			type: progressTypes[step],
+			payload: { field: progressTypes[step], value: html },
+		})
+	}
 
-  console.log(value)
+	console.log(value)
 
-  return (
-    <ReactQuill
-      theme={'snow'}
-      modules={modules}
-      formats={formats}
-      onChange={onChange}
-      bounds={'.app'}
-      placeholder={"Hello there..."}
-    />
-
-  )
+	return (
+		<ReactQuill
+			theme={'snow'}
+			modules={modules}
+			formats={formats}
+			onChange={onChange}
+			bounds={'.app'}
+			placeholder={"Hello there..."}
+		/>
+	)
 }
 
 export default CreateInstructions;
